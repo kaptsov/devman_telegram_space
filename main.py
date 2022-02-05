ccdcead55ffa671edb44cbe06d7e3e43bd239e21
kaptsov <kaptsovaa@gmail.com>
2022-02-06 01:20:47 +0400

    ADDED enviroment variable for posting delay

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import datetime
-
 import os
 import requests
 from dotenv import load_dotenv
-
 import telegram
+from os import listdir
+from os.path import isfile
+from os.path import join as joinpath
+import time
 
 
 def download_image(url, directory, filename):
@@ -29,6 +31,7 @@ def fetch_spacex_last_launch(url, directory):
 
     for image_number, image_url in enumerate(images_list):
         image_name = f'spacex_{image_number}.jpg'
+        description = f'SpaceX image number {image_number}'
         download_image(image_url, directory, image_name)
 
 
@@ -53,41 +56,52 @@ def download_nasa_pics(url_list, directory, is_it_epic):
         day = '%02d' % date_time.day
         month = '%02d' % date_time.month
         year = date_time.year
-
-        image_name = f'{directory[:5]}_{year}-' \
-                     f'{month}-{day}-{nasa_num}{file_ext}'
+        description = f'Nasa pic {nasa_num}'
+        image_name = f'{directory[6:10]}_{year}-' \
+                      f'{month}-{day}-{nasa_num}{file_ext}'
 
         if is_it_epic:
             pic_link = f'https://api.nasa.gov/EPIC/archive/natural/' \
-                       f'{year}/{month}/{day}/png/{nasa_link["image"]}.png?api_key=' \
-                       f'{nasa_token}'
+                       f'{year}/{month}/{day}/png/{nasa_link["image"]}' \
+                       f'.png?api_key={nasa_token}'
         else:
             pic_link = nasa_link['url']
 
         download_image(pic_link, directory, image_name)
 
 
-if __name__ == '__main__':
+def upload_images():
 
     load_dotenv()
     telegram_token = os.getenv("TELEGRAM_BOT_TOKEN")
     bot = telegram.Bot(token=telegram_token)
-
-    print(bot.get_me())
     updates = bot.get_updates()
-    print(updates[0])
-    bot.send_message(text='Привет, Оля!', chat_id=-1001681623193)
+    chat_id = updates[0]['my_chat_member']['chat']['id']
+    pic_path = "pics"
+    seconds = int(os.getenv("TIMER_PERIOD", default=86400))
 
-    spacex_dir = "spacex_pic/"
+    for i in listdir(pic_path):
+        for inner_pic_path in listdir(f'{pic_path}/{i}'):
+            photo_path = f'{pic_path}/{i}/{inner_pic_path}'
+            bot.send_photo(chat_id=chat_id,
+                           photo=open(photo_path, 'rb'),
+                           timeout=2000)
+            time.sleep(seconds)
+
+
+if __name__ == '__main__':
+
+    spacex_dir = "pics/spacex_pic/"
     spacex_url = "https://api.spacexdata.com/v3/launches/67"
     fetch_spacex_last_launch(spacex_url, spacex_dir)
 
-    nasa_pic_dir = 'daily_nasa/'
+    nasa_pic_dir = 'pics/daily_nasa/'
     nasa_url = 'https://api.nasa.gov/planetary/apod?api_key='
     download_nasa_pics(nasa_url, nasa_pic_dir, False)
 
-    nasa_epic_dir = 'epics_nasa/'
+    nasa_epic_dir = 'pics/epics_nasa/'
     nasa_epic_url = 'https://api.nasa.gov/EPIC/api/natural?api_key='
     download_nasa_pics(nasa_epic_url, nasa_epic_dir, True)
 
-
+    while(True):
+        upload_images()
